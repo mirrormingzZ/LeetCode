a726fc8d50ceb3da52d2972e3ce44a3245b9c773
mirrorming <mirrorming@qq.com>
2020-09-21 23:42:31 +0800

    LeetCode_538_Convert_BST_to_Greater_Tree ✔✔✔

--- generate/GeneratePythonFile.py
+++ generate/GeneratePythonFile.py
@@ -1,5 +1,5 @@
-# 创建一个txt文件，文件名为mytxtfile,并向文件写入msg
-def text_create(name, msg=None):
+# 创建一个 py 文件，文件名为 LeetCode_xxx_xxx.py ,并向文件写入 class Solution: ...
+def text_create(name, content=None):
     # 在 leetcode 文件夹下根据 leetcode 的提名创建一个 LeetCode_题号_题名文件
     print("输入名字: {}".format(name))
     # 替换名字的 '. ' 为 _
@@ -8,8 +8,8 @@ def text_create(name, msg=None):
     name = name.replace(' ', '_')
     name = 'LeetCode_' + name + '.py'
     file = open('../leetcode/' + name, 'w')
-    file.write(msg if None else "class Solution:")  # msg也就是下面的Hello world!
+    file.write(content if None else "class Solution:")
     file.close()
 
 
-text_create('78. Subsets')
+text_create('538. Convert BST to Greater Tree')
