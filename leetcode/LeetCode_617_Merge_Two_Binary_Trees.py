ccb11bc442cc03170462ac0fd9e79455bb79bedb
mirrorming <mirrorming@qq.com>
2020-09-24 00:35:33 +0800

    LeetCode_617_Merge_Two_Binary_Trees ✔✔✔

---
+++ leetcode/LeetCode_617_Merge_Two_Binary_Trees.py
@@ -0,0 +1,81 @@
+"""
+617. 合并二叉树
+给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+
+你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+
+示例 1:
+
+输入:
+	Tree 1                     Tree 2
+          1                         2
+         / \                       / \
+        3   2                     1   3
+       /                           \   \
+      5                             4   7
+输出:
+合并后的树:
+	     3
+	    / \
+	   4   5
+	  / \   \
+	 5   4   7
+注意: 合并必须从两个树的根节点开始。
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+    def __str__(self) -> str:
+        return '%s %s %s' % (self.val, self.left, self.right)
+
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+
+        merged = TreeNode(t1.val + t2.val)
+        merged.left = self.mergeTrees(t1.left, t2.left)
+        merged.right = self.mergeTrees(t1.right, t2.right)
+        return merged
+
+
+class Solution2:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not (t1 and t2):
+            return t1 if t1 else t2
+        else:
+            t1.val = t1.val + t2.val
+            t1.left = self.mergeTrees(t1.left, t2.left)
+            t1.right = self.mergeTrees(t1.right, t2.right)
+
+            return t1
+
+
+r = TreeNode(1)
+r.left = TreeNode(2)
+r.right = TreeNode(3)
+r.left.left = TreeNode(4)
+r.left.right = TreeNode(5)
+r.right.left = TreeNode(6)
+r.right.right = TreeNode(7)
+
+r2 = TreeNode(5)
+r2.left = TreeNode(3)
+r2.right = TreeNode(13)
+r2.left.left = TreeNode(1)
+r2.left.right = TreeNode(4)
+r2.right.left = TreeNode(10)
+r2.right.right = TreeNode(15)
+
+s = Solution2()
+res = s.mergeTrees(r, r2)
+print(res)
