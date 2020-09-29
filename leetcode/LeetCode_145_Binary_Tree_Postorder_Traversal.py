ea0068430b84ba7d0f61c6c22ae65f5174669fa7
mirrorming <mirrorming@qq.com>
2020-09-30 00:01:11 +0800

    LeetCode_145_Binary_Tree_Postorder_Traversal✔✔✔

---
+++ leetcode/LeetCode_145_Binary_Tree_Postorder_Traversal.py
@@ -0,0 +1,39 @@
+""""
+给定一个二叉树，返回它的 后序 遍历。
+
+示例:
+
+输入: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+输出: [3,2,1]
+进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+"""
+
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def postorderTraversal(self, root: TreeNode) -> List[int]:
+        result = []
+
+        def travel(node):
+            if node:
+                travel(node.left)
+                travel(node.right)
+                result.append(node.val)
+
+        travel(root)
+        return result
